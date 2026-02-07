6cbb6949e042a7cf735768a2cb211c95d1996231
maavez23 <maavez23cs@student.mes.ac.in>
2026-02-07 19:50:19 +0530

    Update showRoutes.js

--- backend/routes/showRoutes.js
+++ backend/routes/showRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 
-const {
-  addShow,
-  getShows
-} = require("../controllers/showController");
+const { addShow, getShows } = require("../controllers/showController");
 
-router.post("/admin/shows", addShow);
+// multer config (no file save needed now)
+const upload = multer();
+
+router.post("/admin/shows", upload.none(), addShow);
 router.get("/admin/shows", getShows);
 
 module.exports = router;
