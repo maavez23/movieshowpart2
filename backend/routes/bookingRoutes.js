ef8d9ee3c21b0d069850155ea8484a5a288122a8
maavez23 <maavez23cs@student.mes.ac.in>
2026-02-07 12:21:41 +0530

    Update bookingRoutes.js

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { createBooking } = require("../controllers/bookingController");
+app.get("/api/bookings", getBookings);
 
 router.post("/", createBooking);
 
