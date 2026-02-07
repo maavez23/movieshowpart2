216976236c017196427c0744ecd1ae653688c635
maavez23 <maavez23cs@student.mes.ac.in>
2026-02-07 12:20:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
-
+app.get("/api/bookings", getBookings);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log("DB HOST:", process.env.DB_HOST);
