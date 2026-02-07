a5a1ce3ee2cbef8531116820fc6cdd051a689db3
maavez23 <maavez23cs@student.mes.ac.in>
2026-02-07 17:53:29 +0530

    Update showRoutes.js

--- backend/routes/showRoutes.js
+++ backend/routes/showRoutes.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// 🔥 EXACT IMPORT
 const { addShow, getShows } = require("../controllers/showController");
 
-// ROUTES
 router.post("/admin/shows", addShow);
 router.get("/admin/shows", getShows);
 
